792cad5f8d97cd529894fe4b75df5ba0ea4f018a
Ashley <2518117532@qq.com>
2019-03-23 09:33:41 +0800

    changes to Practical5

--- Practical5/DNAsequence.py
+++ Practical5/DNAsequence.py
@@ -15,20 +15,22 @@ g=0
 for i in S:
     if i == "A":
         a=a+1
-for i in S:
-    if i == "T":
+        #a+=1
+    elif i == "T":
         t=t+1
-for i in S:
-    if i == "C":
+    elif i == "C":
         c=c+1
-for i in S:
-    if i == "G":
+    elif i == "G":
         g=g+1
+    else:
+        print("The sequence is wrong.")
+        break
 #print
-print("{","'A'",":",a,",","'T'",":",t,",","'C'",":",c,",","'G'",":",g,"}")
+    print("{","'A'",":",a,",","'T'",":",t,",","'C'",":",c,",","'G'",":",g,"}")
 #draw pie
-import matplotlib.pyplot as plt
-labels='A','T','C','G'
-size=[a,t,c,g]
-plt.pie(size,labels=labels,autopct='%1.1f%%')
-plt.axis('equal')
+    import matplotlib.pyplot as plt
+    labels='A','T','C','G'
+    size=[a,t,c,g]
+    plt.pie(size,labels=labels,autopct='%1.1f%%')
+    plt.axis('equal')
+    plt.title("Pie of the four DNA nucleotide")
--- Practical5/reverse&sorting.py
+++ Practical5/reverse&sorting.py
@@ -9,6 +9,5 @@ Created on Wed Mar 20 09:30:09 2019
 A=input("give me a string of words\n")
 A=A[::-1]
 B=A.split(" ")
-print(B)
 C=sorted(B,reverse=True)
 print(C)
